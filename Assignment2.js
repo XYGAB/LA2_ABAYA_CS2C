b4c8139bf130c34748df8f82b297a4492b303c32
unknown <gabsslemiabaya@gmail.com>
2024-09-21 21:56:30 +0800

    Complete Assignment

---
+++ Assignment2.js
@@ -0,0 +1,28 @@
+//Declaring variable and constant
+var a = 25;
+var b = 20;
+var c = 12;
+var d = "15";
+const e = 15;
+
+//displaying what task is required
+console.log(a+b+c+Number(d)+e);//sum of all declared variable and constant
+
+//comparing tha value of variable d and constant e using relational operation
+console.log(d>e);
+console.log(d<e);
+console.log(d>=e);
+console.log(d<=e);
+console.log(d===e);
+
+//operation of the ff variable
+var subAll = a - b - c - d -e;
+var Mutl_div = (a*c)/e;
+var pwr = e ** c;
+var c_3 = e ** 3;
+
+//result of the following operation
+console.log(subAll);
+console.log(Mutl_div);
+console.log(pwr);
+console.log(c_3);
